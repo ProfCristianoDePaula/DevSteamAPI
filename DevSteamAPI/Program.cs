7038e6238688546c9f7045b81b23cfd155b17b65
Prof. Cristiano <cristiano.paula@docente.senai.br>
2025-02-05 14:41:41 -0300

    Inclusão dos EndPoints do Identity Framework

--- DevSteamAPI/Program.cs
+++ DevSteamAPI/Program.cs
@@ -62,7 +62,9 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
     options.SignIn.RequireConfirmedEmail = false;
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
     options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
     options.Password.RequiredLength = 4;
 })
     .AddEntityFrameworkStores<DevSteamAPIContext>()
